212435e9f0e7121d36386b0cf6e5fcf3670e6df5
Luke Tippmann <1340234+lukeatron@users.noreply.github.com>
2021-02-26 15:34:35 -0500

    Add project files.

---
+++ CashRegister/ChangeTabulator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections.Immutable;
+
+namespace CashRegister
+{
+    public class ChangeTabulator
+    {
+        public Dictionary<Denomination, int> MakeChange(int cents)
+            => cents <= 0 ? throw new ArgumentException($"{nameof(cents)} must be a positive number greater than zero.") :
+                _denominationArray.Aggregate(new Dictionary<Denomination, int>(),
+                    (result, denom) => 
+                    {
+                        if()
+                        return result;
+                    });
+
+        private static readonly ImmutableArray<Denomination> _denominationArray =
+            (Enum.GetValues(typeof(Denomination)) as IEnumerable<Denomination>)
+            .OrderByDescending(d => d)
+            .ToImmutableArray();
+
+        private 
+
+    }
+}
---
+++ CashRegister/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashRegister
+{
+    public enum Denomination
+    {
+        Penny = 1,
+        Nickel = 5,
+        Dime = 10,
+        Quarter = 25,
+        One = 100,
+        Five = 500,
+        Ten = 1000,
+        Twenty = 2000,
+        Fifty = 5000,
+        Hundred = 10000,
+    }
+}
